ecfe9e122be6e6febd8d615070e6c5160b7b5c47
Nargis <nargis.salim@btinternet.com>
2021-06-27 02:58:01 +0100

    first commit

---
+++ magic8Ball.js
@@ -0,0 +1,12 @@
+// Magic Eight Ball
+// Use control flow to predict fortune!
+// User inputs a question and program outputs a random fortune.
+
+// State name (optional)
+let userName = ''
+userName ?
+  console.log(`Hello ${userName}!`):
+  console.log(`Hello! What would you like to ask the Magic Eight Ball?`);
+
+// Ask question
+  const userQuestion = 'I love the cartoon  "Wacky Races"! Who wins in tonight\'s episode?';
